f0eefc60946b02152af1f32c314ab73816924895
ashisnishanka <ak.nishanka@gmail.com>
2024-02-10 10:10:06 +0530

    added terraform code

---
+++ module-1/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_version = ">=0.12"
+}
+
+resource "aws_instance" "ec2_example" {
+
+    ami = "ami-0a7cf821b91bcccbc"
+    instance_type = "t2.micro"
+    #key_name= "aws_key"
+    vpc_security_group_ids = [aws_security_group.main.id]
+
+  user_data = <<-EOF
+      #!/bin/sh
+      sudo apt-get update
+      sudo apt install -y apache2
+      sudo systemctl status apache2
+      sudo systemctl start apache2
+      sudo chown -R $USER:$USER /var/www/html
+      sudo echo "<html><body><h1> hello i dream </h1></body></html>" > /var/www/html/index.html
+      EOF
+}
+
+resource "aws_security_group" "main" {
+    name        = "EC2-webserver-SG-1"
+  description = "Webserver for EC2 Instances"
+
+  ingress {
+    from_port   = 80
+    protocol    = "TCP"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    protocol    = "TCP"
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+/*resource "aws_key_pair" "deployer" {
+  key_name   = "aws_key"
+  public_key = ""
+}*/
